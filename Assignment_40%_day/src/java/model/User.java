ed27f29ecb2c77f89df2bf60b7c3b857b814c4fa
ducdm <ducdoanminh2005@gmail.com>
2025-03-05 11:16:05 +0700

    Create User.java
    Tạo user

---
+++ Assignment_40%_day/src/java/model/User.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author ducdo
+ */
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private int departmentId;
+    private Integer managerId;
+    private List<String> roles;
+
+    public User() {}
+    public User(int userId, String username, String password, String fullName, String email, int departmentId, Integer managerId, List<String> roles) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.departmentId = departmentId;
+        this.managerId = managerId;
+        this.roles = roles;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+    
+}
