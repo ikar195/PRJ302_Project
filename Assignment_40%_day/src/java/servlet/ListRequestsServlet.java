a6a770285a1d4682a6c7cd77da1643bfcf3089ee
ducdm <ducdoanminh2005@gmail.com>
2025-03-19 22:10:12 +0700

    Update ListRequestsServlet.java

--- Assignment_40%_day/src/java/servlet/ListRequestsServlet.java
+++ Assignment_40%_day/src/java/servlet/ListRequestsServlet.java
@@ -7,10 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import model.LeaveRequest;
 import model.User;
 
 @WebServlet("/ListRequests")
@@ -21,7 +18,7 @@ public class ListRequestsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
-            response.sendRedirect("login");
+            response.sendRedirect("login.jsp");
             return;
         }
 
@@ -33,55 +30,27 @@ public class ListRequestsServlet extends HttpServlet {
             page = Integer.parseInt(pageParam);
         }
 
-        // Lấy tổng số bản ghi
+        // Lấy danh sách đơn và tổng số bản ghi
+        List requests;
         int totalRecords;
         if (user.getRoles().contains("Manager")) {
+            requests = leaveRequestDAO.getLeaveRequestsByDepartment(user.getDepartmentId(), page, pageSize);
             totalRecords = leaveRequestDAO.getTotalLeaveRequestsByDepartment(user.getDepartmentId());
-        } else {
+        } 
+        else{
+            requests = leaveRequestDAO.getLeaveRequestsByUser(user.getUserId(), page, pageSize);
             totalRecords = leaveRequestDAO.getTotalLeaveRequestsByUser(user.getUserId());
         }
-        if (user.getDepartmentName().contains("Manager")) {
+        if (user.getDepartmentName().contains("Manager")){
+            requests = leaveRequestDAO.getLeaveRequestsByUser(page, pageSize);
             totalRecords = leaveRequestDAO.getTotalLeaveRequestsByUser();
         }
 
-        // Tính tổng số trang tạm thời để lấy toàn bộ dữ liệu
-        int totalPagesTemp = (int) Math.ceil((double) totalRecords / pageSize);
-
-        // Gộp toàn bộ danh sách từ các trang
-        List<LeaveRequest> allRequests = new ArrayList<>();
-        if (user.getRoles().contains("Manager")) {
-            for (int i = 1; i <= totalPagesTemp; i++) {
-                allRequests.addAll(leaveRequestDAO.getLeaveRequestsByDepartment(user.getDepartmentId(), i, pageSize));
-            }
-        } else {
-            for (int i = 1; i <= totalPagesTemp; i++) {
-                allRequests.addAll(leaveRequestDAO.getLeaveRequestsByUser(user.getUserId(), i, pageSize));
-            }
-        }
-        if (user.getDepartmentName().contains("Manager")) {
-            for (int i = 1; i <= totalPagesTemp; i++) {
-                allRequests.addAll(leaveRequestDAO.getLeaveRequestsByUser(i, pageSize));
-            }
-        }
-
-        // Sắp xếp toàn bộ danh sách: "Inprogress" lên đầu
-        Collections.sort(allRequests, (LeaveRequest r1, LeaveRequest r2) -> {
-            if ("Inprogress".equals(r1.getStatus())) {
-                return -1;
-            } else if ("Inprogress".equals(r2.getStatus())) {
-                return 1;
-            }
-            return 0; // Giữ nguyên thứ tự nếu không có "Inprogress"
-        });
-
-        // Phân trang thủ công
+        // Tính tổng số trang
         int totalPages = (int) Math.ceil((double) totalRecords / pageSize);
-        int startIndex = (page - 1) * pageSize;
-        int endIndex = Math.min(startIndex + pageSize, totalRecords);
-        List<LeaveRequest> paginatedRequests = allRequests.subList(startIndex, endIndex);
 
         // Truyền dữ liệu tới JSP
-        request.setAttribute("requests", paginatedRequests);
+        request.setAttribute("requests", requests);
         request.setAttribute("currentPage", page);
         request.setAttribute("totalPages", totalPages);
         request.setAttribute("pageSize", pageSize);
